9472359057a915e6ae3caca76675841af894de55
Zen Dominik <u20201b980@upc.edu.pe>
2022-06-16 09:16:20 -0500

    Chore: Modification of main.js Add event listener

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -5,3 +5,7 @@ $.get("_header.html", contentToInject => {
 $.get("_footer.html", contentToInject => {
     $("#footer-placeholder").replaceWith(contentToInject);
 });
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
